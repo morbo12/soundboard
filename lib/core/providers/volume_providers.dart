f8acd545d81f629b43c780a52266b310699956c3
Lars Lyxell <lars@lyxell.com>
2025-06-15 00:26:15 +0200

    chore: fixes

---
+++ lib/core/providers/volume_providers.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:soundboard/core/properties.dart';
+import 'package:soundboard/features/screen_home/application/audioplayer/data/class_mainvolume.dart';
+import 'package:soundboard/features/screen_home/application/audioplayer/data/class_system_mainvolume.dart';
+
+final mainVolumeProvider =
+    StateNotifierProvider<SystemVolumeNotifier, SystemVolume>(
+      (ref) =>
+          SystemVolumeNotifier(SystemVolume(vol: SettingsBox().mainVolume)),
+    );
+
+final p1VolumeProvider =
+    StateNotifierProvider<SystemVolumeNotifier, SystemVolume>(
+      (ref) => SystemVolumeNotifier(SystemVolume(vol: SettingsBox().p1Volume)),
+    );
+
+final p2VolumeProvider =
+    StateNotifierProvider<SystemVolumeNotifier, SystemVolume>(
+      (ref) => SystemVolumeNotifier(SystemVolume(vol: SettingsBox().p2Volume)),
+    );
+
+final p3VolumeProvider =
+    StateNotifierProvider<SystemVolumeNotifier, SystemVolume>(
+      (ref) => SystemVolumeNotifier(SystemVolume(vol: SettingsBox().p3Volume)),
+    );
+
+final c1VolumeProvider = StateNotifierProvider<VolumeNotifier, Volume>(
+  (ref) => VolumeNotifier(Volume(vol: SettingsBox().c1InitialVolume)),
+);
+
+final c2VolumeProvider = StateNotifierProvider<VolumeNotifier, Volume>(
+  (ref) => VolumeNotifier(Volume(vol: SettingsBox().c2InitialVolume)),
+);
--- lib/features/screen_home/presentation/board/classes/class_0_player_progress_bar.dart
+++ lib/features/screen_home/presentation/board/classes/class_0_player_progress_bar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:soundboard/core/providers/audioplayers_providers.dart';
 import 'package:soundboard/core/services/jingle_manager/jingle_manager_provider.dart';
-import 'package:soundboard/core/utils/providers.dart';
 import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 
 class PlayerProgressBar extends ConsumerStatefulWidget {
